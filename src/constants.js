d7e6f7c7fb8d454c828d01aad4a83b86198f5d6b
Vinicius Gomes <vinigomescunha@gmail.com>
2020-05-13 00:57:15 -0300

    add constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const serverName = "ML-API";
+
+const countryName = 'Brasil'; // Panamá, Argentina, Costa Rica
+
+const defaultPort = 8090;
+
+export { 
+    countryName, 
+    serverName, 
+    defaultPort 
+}
